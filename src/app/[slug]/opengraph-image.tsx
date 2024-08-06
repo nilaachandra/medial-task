2209af13f9a24371df07266a268cfcdc03445493
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-08-06 19:36:00 +0530

    last fix, hopefully

--- src/app/[slug]/opengraph-image.tsx
+++ src/app/[slug]/opengraph-image.tsx
@@ -33,17 +33,11 @@ export default async function Image({ params }: { params: { slug: string } }) {
 
   return new ImageResponse(
     (
-      <div className="relative w-full h-full bg-black bg-opacity-70 flex flex-col justify-end p-4">
-        <img
-          src={`${websiteURL}${post.image}`}
-          className="absolute inset-0 w-full h-full object-cover"
-          alt=""
-        />
-        <div className="relative bg-black bg-opacity-50 p-4 text-white w-full max-w-full">
-          <h1 className="text-4xl md:text-6xl font-bold leading-tight mb-2">
-            {post.title}
-          </h1>
-          <p className="text-lg md:text-2xl leading-snug">{post.description}</p>
+      <div tw="flex flex-col w-full h-full items-center justify-start relative bg-black relative bg-opacity-70">
+        <img src={`${websiteURL}${post.image}`} tw="absolute z-0 w-full h-full" />
+        <div tw="flex flex-col w-full p-4 absolute z-30 bg-black h-full bg-opacity-50">
+          <h1 tw="text-white text-7xl font-bold">{post.title}</h1>
+          <p tw="text-gray-300 text-4xl line-clamp-2">{post.description}</p>
         </div>
       </div>
     ),
