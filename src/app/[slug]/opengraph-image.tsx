48e166e08fb33832038f5bda9d510d64f360bb7e
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-08-06 17:49:52 +0530

    one more test

--- src/app/[slug]/opengraph-image.tsx
+++ src/app/[slug]/opengraph-image.tsx
@@ -38,10 +38,11 @@ export default async function Image({ params }: { params: { slug: string } }) {
           backgroundImage: `url(${websiteURL}${post.image})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
         }}
-        tw="flex flex-col w-full h-full items-center justify-end bg-white"
+        tw="flex flex-col w-full h-full items-center bg-black bg-opacity-70 justify-start bg-white"
       >
-        <div tw="flex flex-col w-full h-2/5 bg-black bg-opacity-70 p-8">
+        <div tw="flex flex-col w-full h-2/5  p-8">
           <h1 tw="text-white text-4xl font-bold mb-4">{post.title}</h1>
           <p tw="text-gray-300 text-xl line-clamp-2">{post.description}</p>
         </div>
