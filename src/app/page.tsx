29c160372ebb1c9d3253d2b1ba2655998e8e8a1d
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-08-06 13:18:19 +0530

    initial test -1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 import Image from "next/image";
 import Link from "next/link";
 import posts from "../staticData/posts";
@@ -8,14 +7,14 @@ export default function Home() {
   return (
     <div className="grid lg:grid-cols-2 grid-cols-1 gap-2">
       {posts.map((post) => (
-        
+        <div key={post.id}>
           <PostCard
             description={post.description}
             title={post.title}
             imageUrl={post.image}
             slug={post.slug}
           />
-
+        </div>
       ))}
     </div>
   );
